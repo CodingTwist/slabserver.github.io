42df42684f7c51ad8779a294923b72f9782e7c2e
Gaming Twist <sam.hardy2002@gmail.com>
2026-01-12 07:29:54 +0000

    moving to my laptop

--- docs/assets/stylesheets/chart.js
+++ docs/assets/stylesheets/chart.js
@@ -163,12 +163,213 @@ document.addEventListener("DOMContentLoaded", async () => {
     const entry = {
       id: 'world-ping',
       instance: chart,
-      toggle
     };
     charts.push(entry);
     el.closest('.chart-wrap').__chart = entry;
 
   }
 
+  function nexusPlaytimeChart() {
+    const el = document.querySelector('[data-chart="nexus_play"]');
+    if (!el) return;
+
+    const chart = echarts.init(el, getTheme());
+
+
+    const option = {
+      title: {
+        text: 'Nexus Server Awareness & Engagement',
+        subtext: '154 responses',
+        left: 'center'
+      },
+      tooltip: {
+        formatter: (info) => {
+          return `${info.name}: ${info.value}%`;
+        }
+      },
+      series: [
+        {
+          type: 'sunburst',
+          radius: ['15%', '80%'],
+          data: [
+            {
+              name: 'Played on Nexus',
+              value: 26.6,
+              children: [
+                {
+                  name: 'Played recently (≤ 3 months)',
+                  value: 11
+                },
+                {
+                  name: 'Played before (not recent)',
+                  value: 15.6
+                }
+              ]
+            },
+            {
+              name: 'Never played on Nexus',
+              value: 73.4,
+              children: [
+                {
+                  name: 'Knew Nexus existed',
+                  value: 38.3
+                },
+                {
+                  name: "Didn’t know Nexus existed",
+                  value: 35.1
+                }
+              ]
+            }
+          ],
+          label: {
+            rotate: 'radial',
+            formatter: '{b}'
+          },
+          emphasis: {
+            focus: 'ancestor'
+          },
+          levels: [
+            {},
+            {
+              r0: '15%',
+              r: '40%',
+              label: {
+                rotate: 0,
+                fontSize: 14
+              }
+            },
+            {
+              r0: '40%',
+              r: '80%',
+              label: {
+                fontSize: 12
+              }
+            }
+          ]
+        }
+      ]
+    };
+
+
+    chart.setOption(option);
+
+    const entry = {
+      id: 'world-ping',
+      instance: chart,
+    };
+    charts.push(entry);
+
+  }
+
+  function nexusPlaytimeChart2() {
+    const el = document.querySelector('[data-chart="nexus_play2"]');
+    if (!el) return;
+
+    let chart;
+
+
+    const option = {
+      title: {
+        text: 'Nexus Server Awareness & Engagement',
+        subtext: '154 responses',
+        left: 'center'
+      },
+      tooltip: {
+        formatter: (info) => {
+          return `${info.name}: ${info.value}%`;
+        }
+      },
+      series: [
+        {
+          type: 'sunburst',
+          radius: ['15%', '80%'],
+          data: [
+            {
+              name: 'Played on Nexus',
+              value: 26.6,
+              children: [
+                {
+                  name: 'Played recently (≤ 3 months)',
+                  value: 11
+                },
+                {
+                  name: 'Played before (not recent)',
+                  value: 15.6
+                }
+              ]
+            },
+            {
+              name: 'Never played on Nexus',
+              value: 73.4,
+              children: [
+                {
+                  name: 'Knew Nexus existed',
+                  value: 38.3
+                },
+                {
+                  name: "Didn’t know Nexus existed",
+                  value: 35.1
+                }
+              ]
+            }
+          ],
+          label: {
+            rotate: 'radial',
+            formatter: '{b}'
+          },
+          emphasis: {
+            focus: 'ancestor'
+          },
+          levels: [
+            {},
+            {
+              r0: '15%',
+              r: '40%',
+              label: {
+                rotate: 0,
+                fontSize: 14
+              }
+            },
+            {
+              r0: '40%',
+              r: '80%',
+              label: {
+                fontSize: 12
+              }
+            }
+          ],
+        }
+      ]
+    };
+
+
+
+    // Intersection Observer to animate when visible
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          // Enable animation and re-set the chart
+          chart = echarts.init(el, getTheme())
+          chart.setOption(option);
+          const entry = {
+            id: 'world-ping',
+            instance: chart,
+          };
+          charts.push(entry);
+          observer.unobserve(el); // animate only once
+        }
+      });
+    }, { threshold: 0.1 });
+
+    observer.observe(el);
+
+
+
+
+  }
+
+
   worldPlayTimeChart();
+  nexusPlaytimeChart();
+  nexusPlaytimeChart2();
 });
